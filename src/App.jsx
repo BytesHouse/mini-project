02c4a11398f0964c30542d484d6c6e3900e9921b
Andrei Lihtgolt <andrei.lihtgolt@actionengine.com>
2024-10-31 19:02:00 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+export function foo() {
+  console.log('Hello World!')
+}
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello World!</h1>
+    </>
+  )
+}
+
+export default App
